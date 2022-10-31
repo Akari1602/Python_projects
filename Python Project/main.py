a92cd5d6143b4a0077f9a9af8655abbd7be3175c
Ayush Shete <75811912+ayushete02@users.noreply.github.com>
2022-11-01 02:21:00 +0530

    Time Complexcity increase due to more lagging in game

--- Python Project/main.py
+++ Python Project/main.py
@@ -4,12 +4,12 @@ import random
  
 pygame.init()
  
-white = (255, 255, 255)
-yellow = (255, 255, 102)
+white = (250, 255, 255)
+yellow = (245, 255, 102)
 black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
+red = (213, 60, 80)
+green = (0, 245, 0)
+blue = (50, 143, 203)
  
 dis_width = 600
 dis_height = 400
@@ -24,6 +24,7 @@ snake_speed = 15
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
+# score_font = pygame.font.SysFont("comicsansms", 25)
  
  
 def Your_score(score):
@@ -44,6 +45,7 @@ def message(msg, color):
  
 def gameLoop():
     game_over = False
+#     game_over = TRUE
     game_close = False
  
     x1 = dis_width / 2
@@ -113,7 +115,9 @@ def gameLoop():
  
         pygame.display.update()
  
-        if x1 == foodx and y1 == foody:
+#         if x1 == foodx and y1 == foody:
+#             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+#             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
             Length_of_snake += 1
